c7983125b4df2b7f275af8821889eea10c19d552
Héber <heber.f.marques@gmail.com>
2019-09-02 10:29:19 -0300

    Aula 56

--- Mines/App.js
+++ Mines/App.js
@@ -2,45 +2,63 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import params from './src/Params'
 import Field from './src/Components/Field';
+import { createMinedBoard } from './src/Functions';
+import MineField from './src/Components/MineField';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando o Mines!</Text>
-        <Text style={styles.instructions}>Tamanho da Grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
 
-        <Field></Field>
-        <Field opened></Field>
-        <Field opened nearMines={1}></Field>
-        <Field opened nearMines={2}></Field>
-        <Field opened nearMines={3}></Field>
-        <Field opened nearMines={6}></Field>
-        <Field mined></Field>
-        <Field mined opened></Field>
-        <Field mined opened exploded></Field>
-        <Field flagged></Field>
-        <Field flagged opened></Field>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = this.createState()
+    }
+    
+
+    minesAmount = () => {
+        const cols = params.getColumnsAmount()
+        const rows = params.getRowsAmount()
+        return Math.ceil(cols * rows * params.difficultLevel)
+    }
+
+    createState = () => {
+        const cols = params.getColumnsAmount()
+        const rows = params.getRowsAmount()
+        return {
+            board: createMinedBoard(rows, cols, this.minesAmount())
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Iniciando o Mines!</Text>
+                <Text style={styles.instructions}>Tamanho da Grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+
+                <View style={styles.board}>
+                    <MineField board={this.state.board}>
+
+                    </MineField>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    board:{}
 });
